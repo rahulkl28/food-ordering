08db5316f6be46f2a9fb7e95cdbd475815ad12c9
rahulkl28 <rahulkdlwal@gmail.com>
2023-10-31 12:02:19 +0530

    intialize database

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'contact us',
+        'NAME': 'contact',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
